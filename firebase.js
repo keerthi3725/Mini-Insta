14723e6e928805a6cf59d1babdf13674ce15d2e8
keerthi3725 <130570727+keerthi3725@users.noreply.github.com>
2024-09-21 11:27:39 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyBiKShQL39tuhanqji8FHZRyvzv-9lM9co",
+  authDomain: "mini-insta-e9468.firebaseapp.com",
+  projectId: "mini-insta-e9468",
+  storageBucket: "mini-insta-e9468.appspot.com",
+  messagingSenderId: "298083506033",
+  appId: "1:298083506033:web:fd1d092a020ca51aa648d0",
+  measurementId: "G-HNV94ZKS4Z"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics(); 
